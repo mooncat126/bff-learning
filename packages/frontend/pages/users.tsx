36319521dc893dd5ca1cfcc4ae858bbcc2dbd9ad
mooncat126 <xiangyuforwork@gmail.com>
2023-07-31 00:14:35 +0900

    add: delete user function

--- packages/frontend/pages/users.tsx
+++ packages/frontend/pages/users.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { GetServerSideProps } from 'next';
-import { FC } from 'react';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { FC, useEffect, useState } from 'react';
 
 interface User {
   id: number;
@@ -11,18 +11,32 @@ interface UsersProps {
   users: User[];
 }
 
-const Users: FC<UsersProps> = ({ users }) => {
+const Users: FC<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ users: initialUsers }) => {
+  const [users, setUsers] = useState<User[]>(initialUsers);
+
+  useEffect(() => {
+    setUsers(initialUsers);
+  }, [initialUsers]);
+
+  const deleteUser = async (id: number) => {
+    alert(id);
+    await axios.delete(`http://localhost:3001/users/${id}`);
+    const updatedUsers = await axios.get<User[]>('http://localhost:3001/users');
+    setUsers(updatedUsers.data);
+  };
+
   return (
     <div>
-      <h1>Users</h1>
+      <h1>Users:</h1>
       {users.map((user) => (
-        <div>
-         {user.name}
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <button onClick={() => deleteUser(user.id)}>Delete User</button>
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default Users;
 
